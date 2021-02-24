a817b4d23b7aef7421808f2e20b368338f720683
Jonathan <jonathanjampamb@gmail.com>
2021-02-24 18:19:50 -0300

    New Project from PHP and Mysqli

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control</title>
+    <link rel="stylesheet" href="src/style/global.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="campoCentral">
+      <img src="src/img/logoControl.png" alt="logo">
+        <div class="campoButton">
+          <a href="servicos.php" id="button">Serviços</a>
+          <a href="ordemServico.php" id="button">Ordem de Serviços</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ ordemServico.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordem de Serviços</title>
+    <link rel="stylesheet" href="src/style/global.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="campoCentral">
+      <img src="src/img/logoControl.png" alt="logo">
+       <h1>Ordem de Serviços</h1>
+    </div>
+</body>
+</html>
